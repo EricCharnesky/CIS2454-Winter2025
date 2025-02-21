69c0d393789b639d75c7fe3f16c431982b6fa6ca
Eric Charnesky <EricCharnesky@gmail.com>
2025-02-20 19:01:10 -0500

    use root

--- PHPDatabaseProject/models/database.php
+++ PHPDatabaseProject/models/database.php
@@ -3,6 +3,6 @@
 // database server type, location, database name
 $data_source_name = 'mysql:host=localhost;dbname=stock';
 // feels bad, but we don't have time to show a better way
-$username = 'stockuser';
-$password = 'test';
+$username = 'root';
+$password = '';
 $database = new PDO($data_source_name, $username, $password);
